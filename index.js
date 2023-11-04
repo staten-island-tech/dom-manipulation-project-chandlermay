cf5e18eb188e7729756b4735b26c0bb5fdee082b
chandlermay <133597587+chandlermay@users.noreply.github.com>
2023-11-04 16:46:23 -0400

    11.4.1

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const DOMselectors = {
-    button: document.getElementById("btn"),
-    text: document.querySelector("#text"),
-    box: document.getElementById("cardcontainer")
+    button: document.getElementById("submitbtn"),
+    text: document.getElementById("addertitle"),
+    box: document.querySelector(".creator"),
 };
 
-function backgroundandText(background, text){
-    background.style.backgroundColor ="red";
-    text.innerHTML = "This is fun";
+function backgroundandText(background, text) {
+    background.style.backgroundColor = "orange";
+    text.textContent = "This is fun";
 }
 
 backgroundandText(DOMselectors.box, DOMselectors.text);
