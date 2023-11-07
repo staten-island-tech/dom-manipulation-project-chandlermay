328c34a83d3853a73a179646d45aa789946c0d93
chandlermay <133597587+chandlermay@users.noreply.github.com>
2023-11-07 12:31:25 -0500

    11/7

--- index.js
+++ index.js
@@ -4,22 +4,30 @@ const DOMselectors = {
     secondbox: document.getElementById("cardcontainer"),
     destinationname: document.getElementById("destination-name"),
     image: document.getElementById("img-url"),
+    remover: document.querySelector(".rmvbtn"),
 };
 
-// function backgroundandText(background, text) {
-//     background.style.backgroundColor = "orange";
-//     text.textContent = "This is fun";
-// }
-
+//INSERT NEW CARD//
 DOMselectors.subbutton.addEventListener("click", function () {
     const destinationname = DOMselectors.destinationname.value;
     const image = DOMselectors.image.value;
     const cardhtml = `
     <div class="card">
     <h2 class="cardtitle">${destinationname}</h2>
-    <img class="cardimg" src="${DOMselectors.image}">
+    <img class="cardimg" src="${image}"></img>
     <button class="rmvbtn">Remove Vacation Destination</button>
     </div>
     `;
-    DOMselectors.secondbox.insertAdjacentHTML("afterend", cardhtml);
+    DOMselectors.secondbox.insertAdjacentHTML("afterbegin", cardhtml);
 });
+
+//CLEAR INPUT FIELDS//
+DOMselectors.subbutton.addEventListener("click", function (){
+    DOMselectors.destinationname.value="";
+    DOMselectors.image.value="";
+})
+
+//REMOVE BUTTON//
+DOMselectors.remover.addEventListener("click" , function (){
+    
+})
