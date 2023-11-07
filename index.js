659b623514ac183c53a3f58d1766643b35470b03
chandlermay <133597587+chandlermay@users.noreply.github.com>
2023-11-06 23:37:02 -0500

    11/6

--- index.js
+++ index.js
@@ -1,14 +1,25 @@
 const DOMselectors = {
-    button: document.getElementById("submitbtn"),
-    text: document.getElementById("addertitle"),
+    subbutton: document.getElementById("submitbtn"),
     box: document.querySelector(".creator"),
+    secondbox: document.getElementById("cardcontainer"),
+    destinationname: document.getElementById("destination-name"),
+    image: document.getElementById("img-url"),
 };
 
-function backgroundandText(background, text) {
-    background.style.backgroundColor = "orange";
-    text.textContent = "This is fun";
-}
+// function backgroundandText(background, text) {
+//     background.style.backgroundColor = "orange";
+//     text.textContent = "This is fun";
+// }
 
-DOMselectors.button.addEventListener("click", function () {
-    backgroundandText(DOMselectors.box, DOMselectors.text);
+DOMselectors.subbutton.addEventListener("click", function () {
+    const destinationname = DOMselectors.destinationname.value;
+    const image = DOMselectors.image.value;
+    const cardhtml = `
+    <div class="card">
+    <h2 class="cardtitle">${destinationname}</h2>
+    <img class="cardimg" src="${DOMselectors.image}">
+    <button class="rmvbtn">Remove Vacation Destination</button>
+    </div>
+    `;
+    DOMselectors.secondbox.insertAdjacentHTML("afterend", cardhtml);
 });
